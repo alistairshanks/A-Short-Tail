e634eb8da97c5db1a461ce2a923672882009f3fc
cruggle <craigmitch@live.co.uk>
2022-01-16 18:09:05 +0000

    running and idle animation added to fox

--- Cruggle and Ali Game Jam/Assets/Scripts/PlayerMovement.cs
+++ Cruggle and Ali Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
+
     float horizontalMove = 0f;
 
     public float runSpeed = 40f;
     bool jump = false;
+    
 
     // Update is called once per frame
     void Update()
     {
 
         horizontalMove =Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
