70c995208fbb1849c893622851c892a92220aec8
cruggle <craigmitch@live.co.uk>
2022-01-17 14:09:55 +0000

    Animation Timer and improvements

--- Cruggle and Ali Game Jam/Assets/Scripts/PlayerMovement.cs
+++ Cruggle and Ali Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -6,31 +6,80 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
 
     float horizontalMove = 0f;
 
     public float runSpeed = 40f;
     bool jump = false;
-    
+
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-        horizontalMove =Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        
+
+
+
         if (Input.GetButtonDown("Jump"))
+
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
+
+        }
+
+        if (timerIsRunning)
+        {
+
+
+            if (timeRemaining > 0)
+
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+
+            else
+            {
+                Debug.Log("time has run out");
+
+                timeRemaining = 0;
+
+                timerIsRunning = false;
+
+                animator.SetBool("TimePassedSleep", true);
+
+            }
+
+
         }
     }
 
-    private void FixedUpdate()
-    {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
-    }
-}
+        public void Onlanding()
+
+        {
+            animator.SetBool("IsJumping", false);
+        }
+
+
+        private void FixedUpdate()
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        }
+
+
+
+ }
+
+
