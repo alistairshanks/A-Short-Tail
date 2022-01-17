903382b01c4f30a94730337179eed4f03a914221
Ali Shanks <a.shanks@live.com>
2022-01-17 20:06:37 +0000

    added enemy script for enemy patrol on platforms

---
+++ Cruggle and Ali Game Jam/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed;
+
+    private bool movingRight = true;
+
+    public bool goLeft;
+
+    int direction = 1;
+
+    public Transform groundDetection;
+
+    bool isFacingRight = true;
+
+    Rigidbody2D rigidbody2D;
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+
+        Vector2 position = rigidbody2D.position;
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 0.1f);
+        if (groundInfo.collider == false)
+        {
+            goLeft = !goLeft;
+
+            Flip();
+        }
+
+        if (goLeft)
+        {
+            position.x = position.x + Time.deltaTime * speed * -direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+
+
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        isFacingRight = !isFacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+}
