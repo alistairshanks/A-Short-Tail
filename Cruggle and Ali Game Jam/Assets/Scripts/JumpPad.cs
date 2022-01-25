568f05a0f8656204ba7b694cce66d23589e7e4b2
Ali Shanks <a.shanks@live.com>
2022-01-25 17:23:30 +0000

    updated jumppad script with mushroom animation bools

--- Cruggle and Ali Game Jam/Assets/Scripts/JumpPad.cs
+++ Cruggle and Ali Game Jam/Assets/Scripts/JumpPad.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     private float bounce = 20f;
+    public Animator animator;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+
+            animator.SetBool("static", false);
+            animator.SetBool("jump", true);
         }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        animator.SetBool("jump", false);
+        animator.SetBool("static", true);
     }
 }
